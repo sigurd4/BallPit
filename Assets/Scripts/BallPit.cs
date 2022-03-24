088286c09aedc9f74b1b883c6e24e0dacba391ae
sigurd4 <sigurd-s@online.no>
2022-03-24 03:25:14 +0100

    Attempt 2

--- Assets/Scripts/BallPit.cs
+++ Assets/Scripts/BallPit.cs
@@ -11,7 +11,7 @@ public class BallPit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
